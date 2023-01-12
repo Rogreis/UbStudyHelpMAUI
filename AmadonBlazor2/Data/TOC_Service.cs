5e05debd56a43a8cc6cf83b2d12ef203c0481fcb
Rogério Reis da Silva <rogreis@gmail.com>
2023-01-12 18:28:46 +0000

    TOC starting to work - linking new paper is still missing

---
+++ AmadonBlazor2/Data/TOC_Service.cs
@@ -0,0 +1,15 @@
+﻿using AmadonBlazorLibrary.Helpers;
+using AmadonBlazorLibrary.UbClasses;
+
+namespace AmadonBlazor2.Data
+{
+    public class TOC_Service
+    {
+        public static Task<TOC_Table> GetTocTableAsync(bool useRightTranslation)
+        {
+            TOC_Table table = useRightTranslation ? StaticObjects.Book.RightTranslation.TOC : StaticObjects.Book.LeftTranslation.TOC;
+            table.Title = $"Tabela de conteúdos em {DateTime.Now}";
+            return Task.FromResult(table);
+        }
+    }
+}
