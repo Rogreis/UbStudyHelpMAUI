1f29c54b2474a9bdf43006e6e4111278684e104a
Rogério Reis da Silva <rogreis@gmail.com>
2023-03-12 12:42:04 +0000

    Navigation to left pages using event

---
+++ Amadon/AmadonEvents.cs
@@ -0,0 +1,21 @@
+﻿using Amadon.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amadon
+{
+    internal delegate void LeftColumnControl(string controlName);
+
+    internal static class AmadonEvents
+    {
+        public static event LeftColumnControl OnLeftColumnControl;
+
+        public static void LeftColumnControl(string controlName)
+        {
+            OnLeftColumnControl?.Invoke(controlName);
+        }
+    }
+}
