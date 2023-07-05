f97b8ecf9ec171ba0e8502be8551e9d7304d7127
Rogério Reis da Silva <rogreis@gmail.com>
2023-07-05 16:33:43 +0100

    Testing online help

---
+++ Amadon/Services/HelpService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Resources;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using AmadonStandardLib.Helpers;
+
+namespace Amadon.Services
+{
+    internal static class HelpService
+    {
+        public static string GetHtml()
+        {
+            try
+            {
+                string helpText = File.ReadAllText("Docs//SearchHelp.md");
+                return MarkdownToHtml.Convert(helpText);
+            }
+            catch (Exception ex)
+            {
+                return $"<p>Error: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ AmadonStandardLib/Helpers/MarkdownToHtml.cs
@@ -0,0 +1,14 @@
+﻿using Markdig;
+
+namespace AmadonStandardLib.Helpers
+{
+    public class MarkdownToHtml
+    {
+        public static string Convert(string markdown)
+        {
+            var pipeline = new MarkdownPipelineBuilder().UseAdvancedExtensions().Build();
+            return Markdown.ToHtml(markdown, pipeline);
+        }
+
+    }
+}
