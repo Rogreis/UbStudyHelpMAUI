ec7f6346267ad513f9a7b22cbdac083feaad58c8
rogeriosilva <rogerio.silva@ctw.bmwgroup.com>
2023-12-29 18:11:00 +0000

    Changes to workflows

---
+++ Amadon/Classes/WebViewPage.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+namespace Amadon.Classes
+{
+    internal class WebViewPage : ContentPage
+    {
+        public WebViewPage(string url)
+        {
+            WebView webView = new()
+            {
+                Source = new UrlWebViewSource { Url = url },
+            };
+
+            // Create and set up the Grid
+            Grid grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Star });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Star });
+
+            // Add the WebView to the Grid
+            grid.Children.Add(webView);
+
+            // Set the Grid as the content of the page
+            Content = grid;
+        }
+    }
+}
